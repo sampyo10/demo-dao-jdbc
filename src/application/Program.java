2be2d39ac64030efa0586a48f5c254cf937c5cd8
Igor Sampaio <igorsampaio9103@gmail.com>
2024-03-14 15:29:59 -0300

    update impelemtation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha");
+		sellerDao.update(seller);
+		System.out.println("update completed");
+		
 	}
 }
